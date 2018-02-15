b978f59c6a140560b5b9eb4076793ecbdd5f274a
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-15 14:29:47 +0100

    remove task_id field from qacct; there is a taskid field already

--- R/qacct.R
+++ R/qacct.R
@@ -34,7 +34,7 @@ qacct_remote <- function(remote, job_id) {
       strs <- out[seq(breaks[[i]]+1, breaks[[i+1]]-1)]
       names <- gsub(" *$", "", stringr::str_sub(strs, 1, 12))
       values <- gsub(" *$", "", stringr::str_sub(strs, 14, -1))
-      data_frame(job_id, task_id = i, name = names, value = values)
+      data_frame(job_id, name = names, value = values)
     })) %>% spread(name, value)
   }
 }
