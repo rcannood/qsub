b6004f053a857572fac25dd8e414614a494be891
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-21 18:18:32 +0200

    add simple test for setting and getting of default qsub config

---
+++ tests/testthat/test-config_file_location.R
@@ -0,0 +1,15 @@
+context("config_file_location")
+
+test_that("should be able to set and get a default qsub config", {
+  loc <- config_file_location()
+
+  prev <- get_default_qsub_config()
+  on.exit(set_default_qsub_config(prev))
+
+  config <- create_qsub_config(remote = "foo", local_tmp_path = "/help", remote_tmp_path = "/bar")
+  set_default_qsub_config(config)
+
+  new <- get_default_qsub_config()
+
+  expect_equal(new, config)
+})
