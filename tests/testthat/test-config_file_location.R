e60fdc9b8f2c53103333aadcb0247557b25030b6
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2018-06-22 04:53:56 +0200

    extend test

--- tests/testthat/test-config_file_location.R
+++ tests/testthat/test-config_file_location.R
@@ -1,8 +1,11 @@
 context("config_file_location")
 
-test_that("should be able to set and get a default qsub config", {
+test_that("config_file_location returns a character", {
   loc <- config_file_location()
+  expect_is(loc, "character")
+})
 
+test_that("should be able to set and get a default qsub config", {
   if (file.exists(config_file_location())) {
     prev <- get_default_qsub_config()
     on.exit(set_default_qsub_config(prev))
