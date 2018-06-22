49879583838f1850d066502842b47ce50a6e3bfd
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2018-06-22 05:23:10 +0200

    remove config file if none was present before the test

--- tests/testthat/test-config_file_location.R
+++ tests/testthat/test-config_file_location.R
@@ -8,8 +8,10 @@ test_that("config_file_location returns a character", {
 test_that("should be able to set and get a default qsub config", {
   if (file.exists(config_file_location())) {
     prev <- get_default_qsub_config()
-    on.exit(set_default_qsub_config(prev))
+  } else {
+    prev <- NULL
   }
+  on.exit(set_default_qsub_config(prev))
 
   config <- create_qsub_config(remote = "foo", local_tmp_path = "/help", remote_tmp_path = "/bar")
   set_default_qsub_config(config)
