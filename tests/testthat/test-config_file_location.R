cc0183bc031a1ed9ca23e26f5d2b9714b05abab8
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-25 13:13:44 +0200

    test whether temporary qsub configs indeed get saved

--- tests/testthat/test-config_file_location.R
+++ tests/testthat/test-config_file_location.R
@@ -13,10 +13,17 @@ test_that("should be able to set and get a default qsub config", {
   }
   on.exit(set_default_qsub_config(prev))
 
+  # test whether you can save a config
   config <- create_qsub_config(remote = "foo", local_tmp_path = "/help", remote_tmp_path = "/bar")
   set_default_qsub_config(config)
+  expect_equal(get_default_qsub_config(), config)
 
-  new <- get_default_qsub_config()
+  # test whether you can save a config non-permanently
+  config2 <- create_qsub_config(remote = "foo2", local_tmp_path = "/help2", remote_tmp_path = "/bar2")
+  set_default_qsub_config(config2, permanent = FALSE)
+  expect_equal(get_default_qsub_config(), config2)
 
-  expect_equal(new, config)
+  # test whether you can revert afterwards
+  set_default_qsub_config(NULL, permanent = FALSE)
+  expect_equal(get_default_qsub_config(), config)
 })
