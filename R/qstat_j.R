25b8557b67feab40ceec14be4daeb5b17067b266
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-25 09:49:06 +0200

    move function

--- R/qstat_j.R
+++ R/qstat_j.R
@@ -1,43 +1,46 @@
-#' Run qacct on remote
+#' Run qstat on remote
 #'
 #' @param qsub_config The config
 #'
 #' @export
-qacct <- function(qsub_config) {
+qstat_j <- function(qsub_config) {
   if (!"job_id" %in% names(qsub_config)) {
     stop(sQuote("qsub_config"), " is not a qsub handle.")
   }
   job_id <- qsub_config$job_id
-  if (is_job_running(qsub_config)) {
-    warning("job ", job_id, " is still running.")
+  if (!is_job_running(qsub_config)) {
+    warning("job ", job_id, " is not running.")
   }
 
   remote <- qsub_config$remote
 
-  qacct_remote(remote, job_id)
+  qstat_j_remote(remote, job_id)
 }
 
-#' Run qacct on remote
+#' Run qstat on remote
 #'
 #' @param remote The remote
 #' @param job_id The job_id of the job
 #'
 #' @importFrom stringr str_sub
 #' @export
-qacct_remote <- function(remote, job_id) {
-  out <- run_remote(paste0("qacct -j ", job_id), remote)$stdout
-  if (grepl("job id \\d* not found", out)[[1]]) {
+qstat_j_remote <- function(remote, job_id) {
+  out <- run_remote(paste0("qstat -j ", job_id), remote)$stdout
+
+  if (grepl("Following jobs do not exist:", out)[[1]]) {
     NULL
   } else {
+    name <- NULL ## appease r cmd check
+
     breaks <- c(which(grepl("======", out)), length(out)+1)
     map_df(seq_len(length(breaks)-1), function(i) {
       strs <- out[seq(breaks[[i]]+1, breaks[[i+1]]-1)]
       data_frame(
         job_id,
-        row_number_i = i,
-        name = gsub(" *$", "", stringr::str_sub(strs, 1, 12)),
-        value = gsub(" *$", "", stringr::str_sub(strs, 14, -1))
-      )
+        task_id = i,
+        name = gsub(": *$", "", stringr::str_sub(strs, 1, 27)),
+        value = gsub(" *$", "", stringr::str_sub(strs, 29, -1))
+      ) %>% filter(!grepl("^ *$", name))
     }) %>%
       spread_("name", "value")
   }
