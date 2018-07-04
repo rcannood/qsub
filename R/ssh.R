30f1bd150a05b7d4aa3f798a9370ebdbf2b8ee87
zouter <wouter.saelens@gmail.com>
2018-07-04 10:01:36 +0200

    small fix

--- R/ssh.R
+++ R/ssh.R
@@ -22,17 +22,17 @@ fetch_hostname_from_config <- function(host) {
 
       rel_config <- ssh_config[seq(hostname_match + 1, end - 1)]
 
-      rel_hostname <- rel_config %>% keep(~ str_detect("^ *HostName "))
+      rel_hostname <- rel_config %>% keep(~ str_detect(., "^ *HostName "))
       if (length(rel_hostname) == 1) {
         hostname <- rel_hostname %>% str_replace_all("^ *HostName *([^ ]*).*$", "\\1")
       }
 
-      rel_username <- rel_config %>% keep(~ str_detect("^ *User "))
+      rel_username <- rel_config %>% keep(~ str_detect(., "^ *User "))
       if (length(rel_hostname) == 1) {
         username <- rel_username %>% str_replace_all("^ *User *([^ ]*).*$", "\\1")
       }
 
-      rel_port <- rel_config %>% keep(~ str_detect("^ *Port "))
+      rel_port <- rel_config %>% keep(~ str_detect(., "^ *Port "))
       if (length(rel_hostname) == 1) {
         port <- rel_port %>% str_replace_all("^ *Port *([^ ]*).*$", "\\1")
       }
