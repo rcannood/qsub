64e2a0f7169bf61f1ab4dae2c361caaf58441217
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-09 15:40:14 +0200

    fix error when verbose = TRUE

--- R/ssh.R
+++ R/ssh.R
@@ -61,7 +61,7 @@ create_ssh_connection <- function(remote) {
 #'
 #' @export
 run_remote <- function(command, remote = FALSE, args = character(), verbose = FALSE, shell = FALSE) {
-  if (verbose) cat("# ", gsub("\n", "\n# ", cmd), "\n", sep = "")
+  if (verbose) cat("# ", gsub("\n", "\n# ", command), "\n", sep = "")
 
   time1 <- Sys.time()
 
