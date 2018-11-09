80f313af643e531495528d2778f061bba87cba9d
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-09 12:47:51 +0100

    add map2 to imports

--- R/package.R
+++ R/package.R
@@ -4,7 +4,7 @@
 #' @import tidyr
 #' @importFrom stringr str_replace str_replace_all str_detect str_sub
 #' @importFrom glue glue
-#' @importFrom purrr %>% %||% map map_df map_chr map_lgl map_int map_dbl keep set_names
+#' @importFrom purrr %>% %||% map map2 map_df map_chr map_lgl map_int map_dbl keep discard set_names
 #' @importFrom methods is
 #'
 #' @docType package
